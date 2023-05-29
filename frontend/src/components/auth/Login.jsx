02f881f0a1a034cd726790149d1c62763c59daec
Daniel <daniel.nastase@gmail.com>
2023-05-29 11:07:16 +0300

    poc

---
+++ frontend/src/components/auth/Login.jsx
@@ -0,0 +1,63 @@
+import React, { useEffect } from 'react';
+import axios from 'axios';
+import { useNavigate } from 'react-router-dom';
+import useAuth from '../../hooks/useAuth';
+
+import classes from './AuthForm.module.scss';
+
+function Login() {
+  const { verifyAuth, auth } = useAuth();
+  const navigate = useNavigate();
+
+  useEffect(() => {
+    if (auth) {
+      navigate('/');
+    }
+  }, [auth]);
+
+  const login = async (e) => {
+    console.log('login');
+    e.preventDefault();
+    const email = e.target.email.value;
+    const password = e.target.password.value;
+    console.log('auth before ');
+    console.log(auth);
+    try {
+      const response = await axios.post('/api/auth/login', {
+        email,
+        password,
+      });
+      document.cookie = `access_token=${response.data.token}`;
+      await verifyAuth();
+      navigate('/');
+    } catch (err) {
+      console.log(err);
+      verifyAuth();
+    }
+  };
+  return (
+    <div className={classes.register}>
+      <h1 className={classes.title}>Login</h1>
+      <form className={classes.authForm} onSubmit={login}>
+        <label htmlFor="email">
+          email:
+          <input name="email" type="email" placeholder="email" required />
+        </label>
+        <br />
+        <label htmlFor="password">
+          password:
+          <input
+            name="password"
+            type="password"
+            placeholder="password"
+            required
+          />
+        </label>
+        <br />
+        <button type="submit">Login</button>
+      </form>
+    </div>
+  );
+}
+
+export default Login;
